091842699d5b8109e4692fa71f5056e94901022a
Vivek Verma <imvivekvermaa@gmail.com>
2024-03-31 23:58:45 +0530

    Movie-App

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  document.title= "Movie-App | Homepage"
+  return (
+    <div>
+      <div className='w-[20%] h-full bg-red-10'></div>
+      <div className='w-[80%] h-full bg-red-300'></div>
+    </div>
+  )
+}
+
+export default Home
